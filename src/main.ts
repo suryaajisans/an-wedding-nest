f8e187538239c0acfe135f0c1cff5ac3b9b2c447
Surya Aji <suryaajis330@gmail.com>
2024-09-18 21:52:03 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
 
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Specify allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
+    credentials: true, // Enable credentials if needed (for cookies, etc.)
+  });
+
   app.useLogger(logger)
 
   await app.listen(port);
